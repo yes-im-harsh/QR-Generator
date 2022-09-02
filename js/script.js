02964678c6dd4017983d32f489908a235c9f0664
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-03 01:35:54 +0530

    QR code now showing and also implemented clearUI

--- js/script.js
+++ js/script.js
@@ -4,10 +4,40 @@ const qr = document.getElementById("qrcode");
 const onGenerateSubmit = (e) => {
   e.preventDefault();
 
+  clearUI();
   const url = document.getElementById("url").value;
   const size = document.getElementById("size").value;
 
-  console.log(url, size);
+  if (url === "") {
+    alert("Please entre a URL");
+  } else {
+    showSpinner();
+
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(url, size);
+    }, 1000);
+  }
+};
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+const clearUI = () => {
+  qr.innerHTML = "";
+};
+
+const showSpinner = () => {
+  document.getElementById("spinner").style.display = "block";
+};
+
+const hideSpinner = () => {
+  document.getElementById("spinner").style.display = "none";
 };
 
 form.addEventListener("submit", onGenerateSubmit);
